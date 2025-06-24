80d86a6f89dd2dadac648cd999a2669c2e656518
Erik S. Carlsten <ecarlste@gmail.com>
2025-06-24 11:45:46 -0600

    set up initial prettier config w/ sort imports plugin

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  arrowParens: "always",
+  printWidth: 119,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
+
+export default config;
