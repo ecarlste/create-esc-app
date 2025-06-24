188e870792bde6741cf76b841fa670da884ced70
Erik S. Carlsten <ecarlste@gmail.com>
2025-06-24 13:59:39 -0600

    implement app name parsing via clack prompt

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
-import { intro, outro } from "@clack/prompts";
+#!/usr/bin/env node
+import { intro, isCancel, outro, text } from "@clack/prompts";
+import { validateAppName } from "./utils/validateAppName";
 
-intro(`create-esc-app`);
-console.log("let's run some CLI stuff here...");
-outro(`You're all set!`);
+const main = async () => {
+  intro(`create-esc-app`);
+
+  const appName = await text({
+    message: "What is the name of the project?",
+    placeholder: "my-esc-app",
+    validate: validateAppName,
+  });
+
+  if (isCancel(appName)) {
+    outro("create-esc-app cancelled...");
+  } else {
+    outro(
+      `Your esc-app '${appName.toString()}' hass been successfully created!`,
+    );
+  }
+};
+
+try {
+  main();
+} catch (err) {
+  console.error("Aborting installation...");
+
+  if (err instanceof Error) {
+    console.error(err);
+  } else {
+    console.error(
+      "An unknown error has occurred. Please open an issue on github with the text below:",
+    );
+    console.log(err);
+    process.exit(1);
+  }
+}
